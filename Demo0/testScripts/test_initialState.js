9e2fddbd505ecc1dedca2ef72d6d066d5b76ca22
ZhaoYing <zying1023@gmail.com>
2014-05-06 23:24:26 +0800

    cases for adding operation

--- Demo0/testScripts/test_initialState.js
+++ Demo0/testScripts/test_initialState.js
@@ -1,3 +1,5 @@
+//add testing
+
 var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var window = app.mainWindow();
@@ -12,4 +14,44 @@ test("testing initial state",function(){
 	}else{
 		assertEquals(1,editButton,"the editButton can be tapped");
 	}
+})
+
+UIATarget.onAlert = function onAlert(alert)
+{
+	var title = alert.name();
+	if(title == "New Document"){
+		return true;
+	}
+	return false;
+}
+
+test("test add button",function(){
+	var oldTableCellLength = window.tableViews()[0].cells().length;
+	window.navigationBar().buttons()["Add"].tap();
+	var alert = app.alert();
+	log(alert.name());
+	var createButton = alert.buttons()["Create"].isEnabled();
+	var inputText = alert.textFields()[0].name();
+	if(inputText == null)
+	{
+		assertEquals(0,createButton,"create button cannot be tapped if textField is empty");
+	}else{
+		assertEquals(1,createButton,"");
+	}
+	app.keyboard().typeString("cook");
+	alert.buttons()["Create"].tap();
+	window.navigationBar().buttons()["Documents"].tap();
+	var newTableCellLength = window.tableViews()[0].cells().length;
+	assertEquals(newTableCellLength,oldTableCellLength+1,"items++");
+})
+
+test("test adding the existing item",function(){
+	window.navigationBar().rightButton().tap();
+	var alert = app.alert();
+	app.keyboard().typeString("cook");
+	alert.buttons()["Create"].tap();
+	var oldTableCellLength = window.tableViews()[0].cells().length;
+	var newTableCellLength = window.tableViews()[0].cells().length;
+	assertEquals(newTableCellLength,oldTableCellLength,"items does not change");
+	alert.defaultButton().tap();
 })
