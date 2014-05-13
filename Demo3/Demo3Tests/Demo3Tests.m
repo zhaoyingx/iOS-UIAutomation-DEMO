a274c860882ff2bd06b9d75053d6bb985777a41c
ZhaoYing <zying1023@gmail.com>
2014-05-14 00:00:36 +0800

    add Demo3

---
+++ Demo3/Demo3Tests/Demo3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Demo3Tests.m
+//  Demo3Tests
+//
+//  Created by 赵颖 on 14-5-13.
+//  Copyright (c) 2014年 Ying.Zhao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Demo3Tests : XCTestCase
+
+@end
+
+@implementation Demo3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
