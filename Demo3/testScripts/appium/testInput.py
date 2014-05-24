44e0e9186ffdc93846d5a0b40355829a490a669c
ZhaoYing <zying1023@gmail.com>
2014-05-24 15:26:59 +0800

    update case with appium

--- Demo3/testScripts/appium/testInput.py
+++ Demo3/testScripts/appium/testInput.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*=
 
+# xcodebuild -project example.xcodeproj -sdk iPhoneSimulator
+
 import unittest
 from appium import webdriver
 from time import sleep
@@ -9,19 +11,56 @@ import desired_capabilities
 
 class TestInput(unittest.TestCase):
     def setUp(self):
+        '''
+            execute 'xcodebuild -project example.xcodeproj -sdk iPhoneSimulator' first
+            Then it will generate a *.app file at build/Release-iphonesimulator/
+        '''
         desired_caps = desired_capabilities.get_desired_capabilities('Demo3.zip')
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
     def tearDown(self):
         self.driver.quit()
 
+    def testTextEmpty(self):
+        print "start testing alert...."
+        goButton = self.driver.find_element_by_ios_uiautomation('.buttons()[0]')
+        goButton.click()
+        sleep(2)
+        alert = self.driver.switch_to_alert()
+        self.assertIsNotNone(alert)
+        sleep(2)
+        #dismiss alert
+        alert.accept()
+
+
     def testInput(self):
-       textField = self.driver.find_element_by_ios_uiautomation('.textFields()[0]')
-       textField.click()
-       self.driver.set_value(textField,'http://www.google.com.tw')
-       sleep(5)
-       text = textField.get_attribute('value')
-       self.assertEqual('http://www.google.com.tw',text)
+        print "start testing input and clear...." 
+        textField = self.driver.find_element_by_ios_uiautomation('.textFields()[0]')
+        textField.click()
+        self.driver.set_value(textField,'http://www.google.com.tw')
+        sleep(5)
+        text = textField.get_attribute('value')
+        self.assertEqual('http://www.google.com.tw',text)
+
+        textField.clear()
+        default_text = textField.get_attribute('value')
+        self.assertEqual(textField.get_attribute("value"), default_text)
+
+    def testOpenWebView(self):
+        print "start testing open webview...."
+        textField = self.driver.find_element_by_ios_uiautomation('.textFields()[0]')
+        textField.click()
+        self.driver.set_value(textField,'http://www.douban.com')
+        self.driver.find_element_by_ios_uiautomation('.buttons()[0]').click()
+        sleep(5)
+
+        webview = self.driver.find_elements_by_class_name('UIAWebView')
+        self.assertIsNotNone(webview)
+
+        logo = self.driver.find_elements_by_name('豆瓣')
+        self.assertIsNotNone(logo)
+        self.driver.find_element_by_ios_uiautomation('.navigationBar().buttons()[0]').click()
+
 
 if __name__ == '__main__':
     unittest.main()
