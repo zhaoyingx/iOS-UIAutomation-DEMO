09be9825cc34261840335299f518d955c212e3c8
ZhaoYing <zying1023@gmail.com>
2014-05-26 23:19:35 +0800

    add Demo4

---
+++ Demo4/app/collectionde/collectionde/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  collectionde
+//
+//  Created by 赵颖 on 14-5-26.
+//  Copyright (c) 2014年 Ying.Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
